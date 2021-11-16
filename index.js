82bc111fe207e2fac6216a9af565d66ba53320f3
seenso <>
2021-11-15 15:00:07 -0900

    Passed test for createEmployeeRecord. Working on createEmployeeRecords.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0], //str
+    familyName: array[1], //str
+    title: array[2], //str
+    payPerHour: array[3], //number type
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+};
+
+function createEmployeeRecords(array) {
+  let employeeRecords = [];
+
+  return employeeRecords; //arr of objs
+};
+
+function createTimeInEvent() {};
+
+
+function createTimeOutEvent() {};
+
+function hoursWorkedOnDate() {};
+
+function wagesEarnedOnDate() {};
+
+function allWagesFor() {};
+
+function calculatePayroll() {};
