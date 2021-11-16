99dc8bc0a9a41c2f58dde7924450518766dd8bb4
seenso <>
2021-11-15 20:27:48 -0900

    Passed test for hoursWorkedOnDate()

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ function createTimeInEvent(empObj, dateStr) {
   const dateStamp = dateStr.slice(0, 10);
   const hourStamp = parseInt(dateStr.slice(11, 15));
 
-  //returns employee record
   empObj.timeInEvents.push({type: "TimeIn", hour: hourStamp, date: dateStamp});
   return empObj;
 };
@@ -35,14 +34,37 @@ function createTimeOutEvent(empObj, dateStr) {
   const dateStamp = dateStr.slice(0, 10);
   const hourStamp = parseInt(dateStr.slice(11, 15));
 
-  //returns employee record
   empObj.timeOutEvents.push({type: "TimeOut", hour: hourStamp, date: dateStamp});
   return empObj;
 };
 
-function hoursWorkedOnDate(empObj, date) {};
+function hoursWorkedOnDate(empObj, dateStr) {
+  let timeIn = 0;
+  let timeOut = 0;
+  const timeInArr = empObj.timeInEvents; //arr of objs
+  const timeOutArr = empObj.timeOutEvents; //arr of objs
 
-function wagesEarnedOnDate(empObj, date) {};
+  //find time in for dateStr
+  for (let i = 0; i < timeInArr.length; i ++) {
+    if(timeInArr[i].date === dateStr) {
+      timeIn += timeInArr[i].hour;
+    }
+  }
+
+  //find time out for dateStr
+  for (let j = 0; j < timeOutArr.length; j++) {
+    if(timeOutArr[j].date === dateStr) {
+      timeOut += timeOutArr[j].hour;
+    }
+  }
+  
+  //calculate hours worked
+  return (timeOut - timeIn)/100; //convert from military HHMM time to decimal
+};
+
+function wagesEarnedOnDate(empObj, dateStr) {
+  // return hoursWorkedOnDate(empObj, dateStr) * empObj.payPerHour;
+};
 
 function allWagesFor(empObj) {};
 
